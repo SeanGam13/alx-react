fdc7af56afaa88e800be6ffd25389efc61828d74
SeanGam13 <122937338+SeanGam13@users.noreply.github.com>
2024-04-12 02:25:04 +0200

    fix

--- 0x03-React_component/task_0/dashboard/src/Header/Header.js
+++ 0x03-React_component/task_0/dashboard/src/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import holbertonLogo from '../assets/holberton_logo.jpg';
-import './header.css';
+import './Header.css';
+import logo from '../assets/holberton-logo.jpg';
 
 function Header() {
-    return (
-        <header className="App-header">
-            <img src={holbertonLogo} alt="holberton-logo"></img>
-            <h1>School dashboard</h1>
-        </header>
-    );
+  return (
+    <header className='header'>
+      <img src={logo} alt='logo' />
+      <h1>School dashboard</h1>
+    </header>
+  );
 }
 
 export default Header;
